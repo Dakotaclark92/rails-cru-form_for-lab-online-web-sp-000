454d5525a599c0de06b590b652272a18b00a6dcc
Dakota Clark <dakotaclark92@yahoo.com>
2019-10-03 02:35:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :songs, except: [:update, :destroy]
+  resources :genres, except: [:update, :destroy]
+  resources :artists, except: [:update, :destroy]
+end
+
 end
